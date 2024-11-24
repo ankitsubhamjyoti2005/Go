b4ee16f4cd3f8be5fb29e1e1405b18ac3bf53173
ankitsubhamjyoti2005 <ankitsubhamjyoti2005@users.noreply.github.com>
2024-11-24 16:32:40 +0530

    print in go

---
+++ Learning/main.go
@@ -0,0 +1,12 @@
+package main 
+import "fmt"
+
+
+func main(){
+	fmt.Println("hello world")
+	fmt.Println("Weelcomr to the booking app ")
+
+	
+	var conf_name = "go conference"
+	fmt.Println(conf_name + "is the variable name")
+}
